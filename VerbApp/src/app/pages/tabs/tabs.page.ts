f789648af24b9360fca55fde6665517798ac0341
paola4 <arevalo@ualberta.ca>
2024-03-29 22:36:12 -0600

    Implement listening for screen size to tabs Added a service to automatically change the current navigation (tab) bar to the top when we are in desktop view. The bar remains at the bottom in mobile view.

--- VerbApp/src/app/pages/tabs/tabs.page.ts
+++ VerbApp/src/app/pages/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreensizeService } from '../../services/screensize.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
+  isDesktop: boolean;
 
-  constructor() { }
+  // constructor() { }
+  constructor(private screensizeService: ScreensizeService) {
+    this.screensizeService.isDesktopView().subscribe(isDesktop => {
+      if (this.isDesktop && !isDesktop) {
+        // Reload because our routing is out of place
+        window.location.reload();
+      }
+
+      this.isDesktop = isDesktop;
+    });
+  }
 
   ngOnInit() {
   }
