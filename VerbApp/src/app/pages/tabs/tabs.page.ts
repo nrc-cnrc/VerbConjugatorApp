447b31c27f6f6392e7af5a362c42d111f8fa3dde
Delaney <delaney.lothian@gmail.com>
2025-01-14 15:47:02 -0700

    feature: tabs change position based on screen size

--- VerbApp/src/app/pages/tabs/tabs.page.ts
+++ VerbApp/src/app/pages/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreensizeService } from 'src/app/services/screensize/screensize.service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsPage implements OnInit {
 
-  constructor() { }
+  isDesktop: boolean;
+
+  constructor(private screensizeService: ScreensizeService) { 
+
+    this.screensizeService.isDesktopView().subscribe(isDesktop => {
+      // if (this.isDesktop && !isDesktop){
+      //   // Reload because routing is out of place
+      //   window.location.reload();
+      // }
+
+      this.isDesktop = isDesktop;
+    });
+  }
 
   ngOnInit() {
   }
