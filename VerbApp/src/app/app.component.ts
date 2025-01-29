988449a867a9efe025463ec98c3ea4a23ee9245c
Delaney <delaney.lothian@gmail.com>
2025-01-29 15:09:13 -0700

    ion-cards are properly sized now to new mobile standard, result box sized and added as well

--- VerbApp/src/app/app.component.ts
+++ VerbApp/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@awesome-cordova-plugins/splash-screen/ngx';
 import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
+import { ScreensizeService } from './services/screensize/screensize.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private screensizeService: ScreensizeService
   ) {
     this.initializeApp();
   }
@@ -22,6 +24,14 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.screensizeService.onResize(this.platform.width());
     });
   }
+
+  // Since this calls the service ever time the window is resized, the service has a distinctUntilChanged operator
+  @HostListener('window:resize', ['$event'])
+  private onResize(event) {
+    this.screensizeService.onResize(event.target.innerWidth);
+  }
+
 }
