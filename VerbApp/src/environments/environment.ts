f34b0583ae78c64587564b40dd114826aa5ab618
Aidan Pine <hello@aidanpine.ca>
2025-05-13 14:43:56 -0700

    fix: update environment file

--- VerbApp/src/environments/environment.ts
+++ VerbApp/src/environments/environment.ts
@@ -5,12 +5,16 @@
 export const environment = {
   production: false,
   ttsConfig: {
-    apiUrl: "https://unbq-sgile-text-to-speech.hf.space/gradio_api/queue/", // 👈 your TTS backend endpoint
+    apiUrl: "https://unbq-sgile-text-to-speech.hf.space", // 👈 your TTS backend endpoint
     enableTTS: true, // 👈 set this to false if you want to disable your TTS in certain deployment environments. Note, if this is disabled, your EveryVoice components will not render and TTS will not be accessible from your application.
     developmentBearerToken: "", // 👈 OPTIONAL authentication token if required by your API. It will be treated as an Authorization Bearer token
     speakerID: "tina", // 👈 OPTIONAL speaker id.
-    requiresAuth: false,
+    requiresAuth: true,
     diffusionSteps: 3, // 👈  OPTIONAL: number of diffusion steps
+    middlewareEndpoint: "http://localhost:3000/tts",
+    domain: "",
+    clientId: "",
+    audience: "",
   },
 };
 
