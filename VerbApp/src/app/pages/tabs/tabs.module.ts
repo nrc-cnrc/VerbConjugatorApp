2dcf2bab8e8aa3cbe9895673379a1f4c9a40a461
Aidan Pine <hello@aidanpine.ca>
2025-05-13 14:43:05 -0700

    feat: add profile and authentication

--- VerbApp/src/app/pages/tabs/tabs.module.ts
+++ VerbApp/src/app/pages/tabs/tabs.module.ts
@@ -3,19 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-
+import { AuthService } from '@auth0/auth0-angular';
+import { AUTH0_INSTANCE } from '@everyvoice/every-voice';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    TabsPageRoutingModule
+  imports: [CommonModule, FormsModule, IonicModule, TabsPageRoutingModule],
+  declarations: [TabsPage],
+  providers: [
+    {
+      provide: AUTH0_INSTANCE,
+      useExisting: AuthService, // Use the existing instance
+    },
   ],
-  declarations: [TabsPage]
 })
 export class TabsPageModule {}
