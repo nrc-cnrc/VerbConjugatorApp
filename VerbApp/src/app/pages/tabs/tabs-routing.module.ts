cd15be7ea91cdb03f1fbe9fa4dbcc637c27f7119
Aidan Pine <hello@aidanpine.ca>
2025-05-13 14:43:05 -0700

    feat: add profile and authentication

--- VerbApp/src/app/pages/tabs/tabs-routing.module.ts
+++ VerbApp/src/app/pages/tabs/tabs-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { TabsPage } from './tabs.page';
+import { TabsPage } from "./tabs.page";
+import { AuthGuard } from "@auth0/auth0-angular";
+import { UserProfileComponent } from "../../components/profile/profile.component";
 
 const routes: Routes = [
   {
@@ -9,24 +11,36 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-          path: 'about',
-          loadChildren: () => import('../about/about.module').then( m => m.AboutPageModule)
-        },
-        {
-          path: 'conjugator',
-          loadChildren: () => import('../conjugator/conjugator.module').then( m => m.ConjugatorPageModule)
-        },
-        {
-          path: 'instructions',
-          loadChildren: () => import('../instructions/instructions.module').then( m => m.InstructionsPageModule)
-        }
-    ]
+        path: "about",
+        loadChildren: () =>
+          import("../about/about.module").then((m) => m.AboutPageModule),
+      },
+      {
+        path: "conjugator",
+        loadChildren: () =>
+          import("../conjugator/conjugator.module").then(
+            (m) => m.ConjugatorPageModule
+          ),
+      },
+      {
+        path: "instructions",
+        loadChildren: () =>
+          import("../instructions/instructions.module").then(
+            (m) => m.InstructionsPageModule
+          ),
+      },
+      {
+        path: "profile",
+        component: UserProfileComponent,
+        canActivate: [AuthGuard],
+      },
+    ],
   },
   {
-    path: '',
-    redirectTo: '/tabs/conjugator',
-    pathMatch: 'full'
-  }
+    path: "",
+    redirectTo: "/tabs/conjugator",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
